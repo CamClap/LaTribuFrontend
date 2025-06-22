bd5ca1d05152636accd839b3281801eb34b63053
CamClap <c.clappaz@hotmail.fr>
2025-06-22 16:18:16 +0200

    create group ok

--- src/app/components/post-list/post-list.component.ts
+++ src/app/components/post-list/post-list.component.ts
@@ -7,10 +7,13 @@ import { AuthenticationService } from '../../services/authentication.service';
 import { UserService } from '../../services/user.service';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { User } from '../../models/user.model';
+import { Group } from '../../models/group.model';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-post-list',
-  imports: [AsyncPipe, RouterLink],
+  imports: [AsyncPipe, RouterLink, ReactiveFormsModule],
   templateUrl: './post-list.component.html',
   styleUrl: './post-list.component.css'
 })
@@ -19,23 +22,34 @@ export class PostListComponent implements OnInit {
   userService = inject(UserService);
   groupService = inject(GroupService);
   postService = inject(PostService);
+  httpClient = inject(HttpClient);
 
-  person: any = null;
+  user: any = null;
   groups: any[] = [];
   posts: Post[] = [];
   currentGroupName: string = '';
   isLoading = true;
   connectedUser: User | null = null;
+  groupFormGroup: FormGroup;
+
+
+  constructor() {
+    this.groupFormGroup = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    });
+  }
+
 
   ngOnInit() {
     this.authenticationService.connectedUser.subscribe(user => {
-      console.log('user from connectedUser:', user);
+      console.log('Utilisateur connecté reçu:', user);
       if (user) {
         this.userService.findById(user.id).subscribe(fullUser => {
           this.connectedUser = fullUser;
           console.log('connectedUser:', this.connectedUser);
           this.userService.getGroupsByUserId(fullUser.id).subscribe(groups => {
             this.groups = groups;
+            console.log('Groups:', this.groups);
             if (groups.length > 0) {
               this.groupService.setCurrentGroup(groups[0]);
               this.currentGroupName = groups[0].name;
@@ -59,4 +73,24 @@ export class PostListComponent implements OnInit {
       }
     });
   }
+
+  createGroup() {
+    if (this.groupFormGroup.invalid) return;
+
+    const userId = this.authenticationService.getCurrentPersonId();
+
+    const newGroup = {
+      name: this.groupFormGroup.value.name,
+      ownerId: userId,  // ou ce que ton backend attend comme champ
+    };
+
+    this.groupService.save(newGroup).subscribe({
+      next: (savedGroup) => {
+        this.groups.push(savedGroup);
+        this.groupFormGroup.reset();
+      },
+      error: (err) => console.error('Erreur création groupe', err)
+    });
+  }
+
 }
